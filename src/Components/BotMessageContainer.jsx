c8bd09c2bbcbf635ef0efb0f56fbb02d0d4d0785
anugrah18 <anugrah18@gmail>
2025-08-10 16:09:11 -0500

    Further Bot Messages customization

--- src/Components/BotMessageContainer.jsx
+++ src/Components/BotMessageContainer.jsx
@@ -1,10 +1,12 @@
 import BotMessageDefault from "./BotMessageDefault";
 import BotMessageDictionary from "./BotMessageDefinition";
 import BotMessageMemory from "./BotMessageMemory";
+import BotMessageMotivate from "./BotMessageMotivation";
 import BotMessagePersonality from "./BotMessagePersonality";
 import BotMessageStandard from "./BotMessageStandard";
 
 export default function BotMessageContainer({ text, type }) {  
+  
   switch (type) {
     case "personality":
       return <BotMessagePersonality text={text} />;
@@ -12,6 +14,8 @@ export default function BotMessageContainer({ text, type }) {
       return <BotMessageMemory text={text} />;
     case "dictionary_lookup":
       return <BotMessageDictionary text={text} />;
+    case "get_motivation":
+      return <BotMessageMotivate text={text} />;
     case "bot-standard":
       return <BotMessageStandard text={text} />;
     default:
