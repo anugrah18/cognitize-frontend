77141b81cb673b45a3d3cb83d40822c1e00f937b
anugrah18 <anugrah18@gmail>
2025-08-10 21:13:20 -0500

    minor Cosmetic changes

--- src/Components/ThinkingIndicator.jsx
+++ src/Components/ThinkingIndicator.jsx
@@ -1,6 +1,6 @@
 export default function ThinkingIndicator() {
   return (
-    <div className="text-gray-500 italic select-none px-3 py-1 text-sm">
+    <div className="text-gray-500 italic select-none px-3 py-1 text-md">
       Agent is thinking...
     </div>
   );
